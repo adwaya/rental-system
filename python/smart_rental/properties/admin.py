c378d518e8347223f7800786271d95a93f33848a
Adwaya <6140069+adwaya@users.noreply.github.com>
2026-02-01 12:47:41 +0000

    Delete python directory

--- python/smart_rental/properties/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Property
-
-@admin.register(Property)
-class PropertyAdmin(admin.ModelAdmin):
-    list_display = ("title", "location", "price", "property_type")
-    list_filter = ("property_type", "location")
--- python/smart_rental/properties/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("property/<int:pk>/", views.property_detail, name="property_detail"),
-]
